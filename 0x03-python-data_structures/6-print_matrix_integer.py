ef45205bdee6902c107e43bdc288472e009eded1
Xolisakesi <xolisakesi@gmail.com>
2023-12-05 20:09:56 +0200

    pycode style

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
         print()
-    else:
-        for row in range(len(matrix)):
-            for item in range(len(matrix[row])):
-                if item != len(matrix[row]) -1:
-                    endspace=''
-                else:
-                    endspace=""
-                print("{:d}".format(matrix[row][item],end=endspace))
-                print()
