aace3fc5159f76debea4b3b938192ffc564337e3
Xolisakesi <xolisakesi@gmail.com>
2023-12-21 03:29:07 +0200

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,15 @@
-#1/usr/bin/python3
-
-"""Define a class Square."""
-
-def __init__(self, size=0):
-        """
-        Initializes a new instance of the Square class.
-
-        Parameters:
-        - size (int): The size of the square. Default is 0.
-        """
-        self.__size = size
+#!/usr/bin/python3 
+  
+ """Define a class Square.""" 
+  
+  
+ class Square: 
+     """Represent a square.""" 
+  
+     def __init__(self, size): 
+         """Initialize a new Square. 
+  
+         Args: 
+             size (int): The size of the new square. 
+         """ 
+         self.__size = size
