abc5b3da6960585ff879da7623da4bee626a4e6f
Xolisakesi <xolisakesi@gmail.com>
2023-12-21 04:01:27 +0200

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+
+"""Define a MagicClass matching exactly a bytecode."""
+
 import math
 
 class MagicClass:
     """Defines a class that replicates the given bytecode."""
 
     def __init__(self, radius=0):
-        """Initializes the MagicClass with an optional radius."""
+        """
+        Initializes the MagicClass with an optional radius.
+        
+        Args:
+        radius (int or float, optional): The radius of the MagicClass instance (default is 0).
+        
+        Raises:
+        TypeError: If radius is not a number.
+        """
         self.__radius = 0
 
-        if type(radius) is not int and type(radius) is not float:
+        if not isinstance(radius, (int, float)):
             raise TypeError('radius must be a number')
 
         self.__radius = radius
