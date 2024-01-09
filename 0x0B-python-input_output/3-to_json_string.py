87cef6f03e0e70c2737e72e579d3a2dca73d693c
Xolisakesi <xolisakesi@gmail.com>
2024-01-09 21:39:05 +0200

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for to_json_string function."""
+
+def to_json_string(my_obj):
+    """
+    Return the JSON representation of an object (string).
+
+    Args:
+        my_obj: Object to be converted to JSON.
+
+    Returns:
+        str: JSON representation of the input object.
+    """
+    import json
+    return json.dumps(my_obj)
+
