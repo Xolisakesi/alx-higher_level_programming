38863d1bf8ed504bc9a0c663dd3d737113e70dcd
Xolisakesi <xolisakesi@gmail.com>
2024-02-14 21:44:48 +0200

    8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Select all the cities of California using a subquery
+SELECT *
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
+
