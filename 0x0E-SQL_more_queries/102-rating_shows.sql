7e6af4a326da947caecbb613c835cb8dffcb92c1
Xolisakesi <xolisakesi@gmail.com>
2024-02-14 22:36:35 +0200

    102-rating_shows.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating
+-- Aggregates ratings for each show and displays the sum of ratings
+SELECT tv_shows.title, SUM(rating) AS rating_sum
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating_sum DESC;
+
