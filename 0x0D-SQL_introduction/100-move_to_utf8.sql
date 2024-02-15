81524d00fff5aab5b0537d2b08c408c3ca1fbc2a
Xolisakesi <xolisakesi@gmail.com>
2024-02-15 22:28:57 +0200

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
--- Convert hbtn_0c_0 database, first_table table, and name field to UTF8
+-- Converts the entire database hbtn_0c_0 to UTF8.
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
