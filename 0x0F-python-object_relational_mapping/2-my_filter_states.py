357737ef7012ec54417ec64cefc0cf39d81f9dac
Xolisakesi <xolisakesi@gmail.com>
2024-03-17 20:00:46 +0200

    hot fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,42 +1,27 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa.
+Lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
-        sys.exit(1)
-
     # Connect to MySQL server
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-        sys.exit(1)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
 
     # Create cursor object
     cur = db.cursor()
 
     # Execute SQL query
-    try:
-        cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (sys.argv[4],))
-    except MySQLdb.Error as e:
-        print("Error executing SQL query:", e)
-        cur.close()
-        db.close()
-        sys.exit(1)
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
 
     # Fetch and print results
     rows = cur.fetchall()
@@ -46,4 +31,3 @@ if __name__ == "__main__":
     # Close cursor and database connection
     cur.close()
     db.close()
-
