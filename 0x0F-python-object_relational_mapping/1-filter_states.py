4f635e1355a1d3a865539bf09e4c51eaa7a0d6de
Xolisakesi <xolisakesi@gmail.com>
2024-03-17 16:58:30 +0200

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa.
+Script to list all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
 
 
-def filter_states(username, password, database):
-    """
-    Connects to MySQL server and lists states with names starting with 'N'.
+if __name__ == "__main__":
+    # Check if username, password, and database name are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): MySQL database name.
-    """
     try:
         # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                             passwd=password, db=database)
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
         # Create a cursor object to execute queries
-        cursor = db.cursor()
+        cur = db.cursor()
 
-        # Execute the query to select states with name starting with 'N'
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+        # Execute the query to select states with names starting with 'N'
+        cur.execute("""SELECT * FROM states WHERE name
+                    LIKE BINARY 'N%' ORDER BY states.id""")
 
         # Fetch all the rows
-        rows = cursor.fetchall()
+        rows = cur.fetchall()
 
         # Display the results
         for row in rows:
@@ -42,19 +39,5 @@ def filter_states(username, password, database):
     finally:
         # Close cursor and database connection
         if 'db' in locals() and db is not None:
+            cur.close()
             db.close()
-
-
-if __name__ == "__main__":
-    # Check if all 3 arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Extract command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Call function to filter states
-    filter_states(username, password, database)
